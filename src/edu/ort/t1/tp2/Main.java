52acf69235c088a34f635cd04cc9fea108804a62
luckberonne <luckberonne@gmail.com>
2025-05-19 20:45:39 -0300

    tp2 main

--- src/edu/ort/t1/tp2/Main.java
+++ src/edu/ort/t1/tp2/Main.java
@@ -12,6 +12,13 @@ public class Main {
             System.out.println("Seleccione un ejercicio:");
             System.out.println("1 - Ejercicio1 (sumar y pedirNumero)");
             System.out.println("2 - Ejercicio2 (Persona)");
+            System.out.println("3 - Ejercicio3 (Persona)");
+            System.out.println("4 - Ejercicio4 (Instanciando)");
+            System.out.println("5 - Ejercicio5 (¿Dónde vive Fulano?)");
+            System.out.println("6 - Ejercicio6 (Tirando el dado)");
+            System.out.println("7 - Ejercicio7 (Compumundo)");
+            System.out.println("8 - Ejercicio8 (Que grande esta Tarjeta)");
+            System.out.println("9 - Ejercicio9 (Corralito corralón)");
             System.out.println("0 - Salir");
             System.out.print("Opción: ");
             opcion = scanner.nextInt();
@@ -20,6 +27,30 @@ public class Main {
                 case 1:
                     Ejercicio1.ejecutar(scanner);
                     break;
+                case 2:
+                    System.out.println("No es ejecutable...");
+					break;
+				case 3:
+					System.out.println("No es ejecutable...");
+					break;
+				case 4:
+					Ejercicio4.ejecutar();
+					break;
+				case 5:
+					Ejercicio5.ejecutar();
+					break;
+				case 6:
+					Ejercicio6.ejecutar();
+					break;
+				case 7:
+					Ejercicio7.ejecutar();
+					break;
+				case 8:
+					Ejercicio8.ejecutar();
+					break;
+				case 9:
+					Ejercicio9.ejecutar();
+					break;
                 case 0:
                     System.out.println("Saliendo...");
                     break;
